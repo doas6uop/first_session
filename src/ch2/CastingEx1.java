fc15ca849a64598a760afd87c0adc5d25b0a2eec
ds <doas6uop@gmail.com>
2023-05-11 23:54:02 +0900

    :construction: chapter02. 04. 형변환(형변환, 형변환방법, 정수형 간의 형변환, 실수형 간의 형변환, 정수형&실수형 간의 형변환, 자동형변환)

---
+++ src/ch2/CastingEx1.java
@@ -0,0 +1,11 @@
+package ch2;
+
+public class CastingEx1 {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int) d;
+
+        System.out.println("score: " + score);
+        System.out.println("d: " + d); // 형변환후에도 피연산자에는 아무런 변화가 없다.
+    }
+}
